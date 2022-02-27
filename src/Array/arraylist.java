a3e8029a8104f813efb990c09f576b7f27f60fdf
jefrilrahmadoni <jefril.emkom@gmail.com>
2022-02-27 12:09:13 +0700

    new

---
+++ src/Array/arraylist.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class arraylist {
+    public static void main(String[] args) {
+        String nama[] = {"D","O","N","I"};
+
+        //isEmpty()
+        System.out.println(nama[0].isEmpty());
+        System.out.println(nama[1].isEmpty());
+        System.out.println(nama[2].isEmpty());
+        System.out.println(nama[3].isEmpty());
+        //System.out.println(nama[4].isEmpty());
+
+        //size()
+        System.out.println(nama.length);
+    }
+}
