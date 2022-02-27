09b96b109fdc5e085e9cdc265f482b0b0260f373
jefrilrahmadoni <jefril.emkom@gmail.com>
2022-02-27 12:12:23 +0700

    first commit

--- src/Algoritma/luaspersegi.java
+++ src/Algoritma/luaspersegi.java
@@ -3,15 +3,17 @@ package Algoritma;
 import java.util.Scanner;
 
 public class luaspersegi {
+    private static Scanner input;
+
     public static void main(String[] args) {
         int p,l,luas;
-        Scanner input = new Scanner (System.in);
-        
-        System.out.printf("Input nilai Panjang\t= ");
+        input = new Scanner(System.in);
+
+        System.out.printf("Nilai Panjang\t= ");
         p = input.nextInt();
-        System.out.printf("Input nilai Lebar\t= ");
+        System.out.printf("Nilai Lebar\t= ");
         l = input.nextInt();
         luas = p*l;
-        System.out.println("Luas Persegi\t\t= " +luas);
+        System.out.println("Nilai Luas Persegi = " +luas);
     }
 }
