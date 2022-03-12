faa78d3b06bd982f569b02f874df0ca8b30d61fd
Jefril Rahmadoni <jefril.emkom@gmail.com>
2022-03-12 09:02:02 +0700

    Update linked_list.java

--- src/Linked_List/linked_list.java
+++ src/Linked_List/linked_list.java
@@ -13,33 +13,39 @@ public class linked_list {
         elemen.add(30);
         elemen.add(40);
         elemen.add(50);
-        System.out.println("output tambah\t:"+elemen+", Ukuran :" +elemen.size());
+        System.out.println("1.output tambah\t:"+elemen+", Ukuran :" +elemen.size());
         
         //add linkedlist
         elemen.addFirst(9);   
         elemen.addLast(60);
-        System.out.println("output tambah\t:"+elemen+", Ukuran :" +elemen.size());
+        System.out.println("2.output tambah\t:"+elemen+", Ukuran :" +elemen.size());
+
+        elemen.add(3,1);
+        elemen.add(4,2);
+        System.out.println("3.output tambah\t:"+elemen+", Ukuran :" +elemen.size());
 
         data.add(55);
         data.add(55);
         elemen.addAll(data);
-        System.out.println("output tambah\t:"+elemen+", Ukuran :" +elemen.size());
+        System.out.println("4.output tambah\t:"+elemen+", Ukuran :" +elemen.size());
         
         //set linkedlist / sisip
         elemen.set(0,11);
         elemen.set(6,80);
-        System.out.println("output sisip\t:"+elemen+", Ukuran :" +elemen.size());
+        elemen.set(8,33);
+        System.out.println("5.output sisip\t:"+elemen+", Ukuran :" +elemen.size());
         
         //delete linked list
         elemen.removeFirst();
         elemen.removeLast();
-        System.out.println("output hapus\t:"+elemen+", Ukuran :" +elemen.size());
+        System.out.println("6.output hapus\t:"+elemen+", Ukuran :" +elemen.size());
         
         elemen.remove(3);
-        System.out.println("output hapus\t:"+elemen+", Ukuran :" +elemen.size());
+        System.out.println("7.output hapus\t:"+elemen+", Ukuran :" +elemen.size());
 
         //is Empty
-        System.out.println("Is List empty?\t " +elemen.isEmpty());
+        System.out.println("8.Is List empty?\t " +elemen.isEmpty());
         elemen.clear();
+        System.out.println("9.Is List empty?\t " +elemen.isEmpty());
     }
 }
